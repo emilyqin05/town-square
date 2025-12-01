a627cad7ba8d9afcd509c177bd829d9daa6813ba
emilyqin05 <123emilyqin@gmail.com>
2025-12-01 00:52:43 -0800

    mocking api request

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 
 const app = express();
 const PORT = 3001;
+
+app.use(express.json());
+
+const postsRouter: Router = express.Router();
+
+postsRouter.get('/', (req: Request, res: Response) => {
+    // This mocks the data we will eventually fetch from Firestore.
+    res.status(200).json({
+        message: "This is the public list of posts (Mock Data).",
+        data: [
+            { id: "mock-101", title: "Intro to Express", votes: 5 }
+        ]
+    });
+});
+
+app.use('/api/posts', postsRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('testing backend');
